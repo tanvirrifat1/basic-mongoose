94811371916e02d013ff7fbe461ca5d3736ea230
tanvirrifat1e <rifatkhan5567790@gmail.comm>
2023-05-14 13:10:42 +0600

    added more

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,14 +1,28 @@
 import { NextFunction, Request, Response } from "express";
-import { createUserToDB } from "./user.service";
+import { createUserToDB, getUsersFromDB } from "./user.service";
 
 export const createUser = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const user = await createUserToDB();
+  const data = req.body;
+  const user = await createUserToDB(data);
 
   res.status(200).json({
+    status: "success",
+    data: user,
+  });
+};
+
+export const getUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const user = await getUsersFromDB();
+
+  res.status(203).json({
     status: "success",
     data: user,
   });
--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,23 +1,30 @@
+import { IUser } from "./user.interface";
 import User from "./user.model";
 
-export const createUserToDB = async () => {
-  const user = await new User({
-    id: "8526968",
-    roll: "students",
-    password: "8784654",
-    name: {
-      firstName: "MR homes",
-      middleName: "khan",
-      lastName: "akkas ali",
-    },
-    // dateOfBirth: "08/03/2000",
-    gender: "male",
-    contactNo: "014545534654",
-    email: "akkas@gmail.com",
-    emergencyContactNo: "545465456",
-    presentAddress: "garpara",
-    permanentAddress: "dont know",
-  });
+export const createUserToDB = async (payload: IUser): Promise<IUser> => {
+  // creating a new user
+  const user = new User(payload); //User -> class  user -> instance
   await user.save();
   return user;
 };
+export const getUsersFromDB = async () => {
+  const users = await User.find();
+  return users;
+};
+
+// {
+//     id: "8526968",
+//     roll: "students",
+//     password: "8784654",
+//     name: {
+//       firstName: "MR homes",
+//       middleName: "khan",
+//       lastName: "akkas ali",
+//     },
+//     gender: "male",
+//     contactNo: "014545534654",
+//     email: "akkas@gmail.com",
+//     emergencyContactNo: "545465456",
+//     presentAddress: "garpara",
+//     permanentAddress: "dont know",
+//   }
